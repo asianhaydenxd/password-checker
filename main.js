bd261d5e6ed5e30e69796af6713a5ef9a615d1dc
Hayden <asianhaydenxd@gmail.com>
2021-07-02 18:34:08 -0400

    Add basic password checker functionality

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-function toggleVisibility() {
-    let password = document.getElementById('password');
-    let newType = password.getAttribute('type') === 'password' ? 'text' : 'password';
+let password = document.getElementById('password');
 
+let Password = {
+    check: (pass, lengthMin, capitalNeeded, numberNeeded, symbolNeeded) => {
+        return {
+            lengthMet: pass.length >= lengthMin,
+            capitalMet: capitalNeeded && /[A-Z]/.test(pass),
+            numberMet: numberNeeded && /[0-9]/.test(pass),
+            symbolMet: symbolNeeded && /\W/.test(pass)
+        };
+    }
+};
+
+password.addEventListener('input', function() {
+    let check = Password.check(password.value, 8, true, true, true);
+
+    console.log(check);
+
+    updateRequirement('length', check.lengthMet);
+    updateRequirement('capital', check.capitalMet);
+    updateRequirement('number', check.numberMet);
+    updateRequirement('symbol', check.symbolMet);
+});
+
+function updateRequirement(test, check) {
+    document.querySelector(`#${test}-test`).className = check ? 'pass' : 'fail';
+    document.querySelector(`#${test}-test .status`).innerHTML = check ? '✓' : 'O';
+}
+
+function toggleVisibility() {
+    let newType = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', newType);
 }
